fb61a726b1bee39f1a94c04569c0ef0cd9180499
Oleg Shkalikov <maska6293@yandex.ru>
2022-12-26 08:43:37 +0300

    feat: add draw circle, rectangle, square

---
+++ remote-control/src/utils/drawMouse.ts
@@ -0,0 +1,56 @@
+import { mouse, Point, screen } from '@nut-tree/nut-js';
+
+import mouseMove from './moveMouse';
+
+const drawRectangle = async (width: number, length: number): Promise<void> => {
+  await mouseMove('right', length);
+  await mouseMove('down', width);
+  await mouseMove('left', length);
+  await mouseMove('up', width);
+};
+
+const drawCircle = async (
+  radius: number,
+  screenWidth: number,
+  screenHeight: number
+) => {
+  for (let i = 180; i > -180; i -= 0.03) {
+    const x = radius * Math.sin((Math.PI * 2 * i) / 360);
+    const y = radius * Math.cos((Math.PI * 2 * i) / 360);
+    mouse.setPosition(new Point(x + screenWidth / 2, y + screenHeight / 2));
+  }
+  await mouse.setPosition(new Point(screenWidth / 2, screenHeight / 2));
+};
+
+const drawMouse = async (
+  command: string,
+  widthOrRadius: number,
+  length: number
+) => {
+  const [screenHeight, screenWidth] = await Promise.all([
+    screen.height(),
+    screen.width(),
+  ]);
+
+  await mouse.setPosition(new Point(screenWidth / 2, screenHeight / 2));
+
+  mouse.config.mouseSpeed = 500;
+
+  switch (command) {
+    case 'square': {
+      await drawRectangle(widthOrRadius, widthOrRadius);
+      break;
+    }
+    case 'rectangle': {
+      await drawRectangle(widthOrRadius, length);
+      break;
+    }
+    case 'circle': {
+      await drawCircle(widthOrRadius, screenWidth, screenHeight);
+      break;
+    }
+    default:
+  }
+};
+
+export default drawMouse;
--- remote-control/src/ws_server/index.ts
+++ remote-control/src/ws_server/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import { EOL } from 'os';
 import moveMouse from '../utils/moveMouse';
 import getPrintScreen from '../utils/printScreen';
+import drawMouse from '../utils/drawMouse';
 
 dotenv.config();
 
@@ -14,14 +15,17 @@ process.stdout.write(`WSS has been started on port ${port}${EOL}`);
 
 wss.on('connection', (ws) => {
   ws.on('message', async (data: string) => {
-    const [command, value] = data.toString().split(' ');
+    const [command, widthOrRadius, length] = data.toString().split(' ');
 
     if (command.startsWith('mouse')) {
-      moveMouse(command.slice(6), Number(value));
+      moveMouse(command.slice(6), Number(widthOrRadius));
     }
     if (command === 'prnt_scrn') {
       const image = await getPrintScreen();
       ws.send(image);
+    }
+    if (command.startsWith('draw')) {
+      drawMouse(command.slice(5), Number(widthOrRadius), Number(length));
     }
   });
 });
